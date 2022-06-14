f80099507e301b38e567498a4252a1a36309286f
ZainRashid19 <92735725+ZainRashid19@users.noreply.github.com>
2022-06-14 10:40:36 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+function showForm() {
+  if (document.getElementById("contactForm").style.display == "block") {
+    document.getElementById("contactForm").style.display = "none";
+  } else {
+    document.getElementById("contactForm").style.display = "block";
+  }
+};
